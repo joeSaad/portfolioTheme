ed40cc25d53611cc5d053bde1af22c41950414c7
Joe Saad <yousseffhs@yahoo.com>
2015-12-07 18:33:03 -0600

    - fancybox fix
    prevent default scrolling for fancybox images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,13 @@
-    $(".fancybox").fancybox({padding: 0});
+    $(".fancybox").fancybox({
+    	padding: 0,
+    	helpers: {
+    		overlay: {
+      			locked: false
+    		}
+  		}
+    });
 
-    if ($('li.project').length > 1) {
+   /* if ($('li.project').length > 1) {
         $('li.project:gt(0)').hide();
         $('.fa-ellipsis-v').show();
-    };
+    };*/
